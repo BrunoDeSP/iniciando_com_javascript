62c2a025018f97c330568cafd908feb9d131427b
BrunoDeSP <antunesbrunobarbosa1@gmail.com>
2023-09-26 22:47:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+//  1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+    // alert("Hello World")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+    // var valor_um = prompt("Digite um valor")
+    // var valor_dois = prompt("Digite outro valor")
+    // var resultado = Number(valor_um) + Number(valor_dois)
+    // alert('Seu resultado é ' + resultado)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".    
+
+
+    // var valor = prompt('Insira um valor')
+    // if(Number(valor) == valor){
+    //     alert('É um número')
+    // } else {
+    //     alert('Não é um número')
+    // }
+
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+    // var valor = prompt('Insira um valor')
+    // var valor_convertido = String(valor)
+    // console.log(Number(valor_convertido))
+    // if(String(valor) == valor && Number(valor_convertido) != valor_convertido){
+    //     alert('É uma string')
+    // } else {
+    //     alert('Não é uma string')
+    // }
+
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+    // var valor = prompt('Insira um valor')
+    // if(valor == "true" || valor == "false"){
+    //     alert('É um booleano')
+    // } else {
+    //     alert('Não é um booleano')
+    // }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+    // var valor_um = prompt("Insira o primeiro valor")
+    // var valor_dois = prompt("Insira o segundo valor")
+    // var resultado = Number(valor_um) - Number(valor_dois)
+    // alert("Seu resultado é " + resultado)
+
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+    // var valor_um = prompt("Insira o primeiro valor")
+    // var valor_dois = prompt("Insira o segundo valor")
+    // var resultado = Number(valor_um) * Number(valor_dois)
+    // alert("Seu resultado é " + resultado)
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+    // var valor_um = prompt("Insira o primeiro valor")
+    // var valor_dois = prompt("Insira o segundo valor")
+    // var resultado = Number(valor_um) / Number(valor_dois)
+    // alert("Seu resultado é " + resultado)
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+    // var valor_um = prompt("Insira o primeiro valor")
+    // var valor_dois = 2
+    // var resultado = Number(valor_um) % Number(valor_dois)
+    // if(resultado == 0) {
+    //     alert('Este numero é par')
+    // } else {
+    //     alert('Este numero é impar')
+    // }
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+    // var valor_um = prompt("Insira o primeiro valor")
+    // var valor_dois = 2
+    // var resultado = Number(valor_um) % Number(valor_dois)
+    // if(resultado >= 1) {
+    //     alert('Este numero é impar')
+    // } else {
+    //     alert('Este numero é par')
+    // }
